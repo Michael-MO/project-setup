efb2278f0e3ed8c49faec7e9b85f0ea6c7528ce1
Michael-MO <Michael-MO@users.noreply.github.com>
2019-09-02 14:11:04 +0200

    Implement the PlayersList component

--- src/components/Setup/SetupDetails.js
+++ src/components/Setup/SetupDetails.js
@@ -1,38 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
+import PlayersList from "./PlayersList";
+import { CalColumnWidth } from "./../../utils";
 
 const SetupDetails = props => {
   return (
     <React.Fragment>
-      <h4 className="card-title">{props.selectRaid.Name}</h4>
+      {/* <h4 className="card-title">{props.selectRaid.Name}</h4> */}
       <table className="table table-sm">
         <thead className="thead-light">
           <tr>
             <th className="col-1">Players</th>
             {props.selectRaid.Bosses.map(boss => {
               return (
-                <th key={boss.ID}>
-                  <i className="fas fa-skull"></i> {boss.Name}
+                <th
+                  style={{
+                    width: CalColumnWidth(props.selectRaid.Bosses.length)
+                  }}
+                  key={boss.ID}
+                >
+                  {boss.Alias}
                 </th>
               );
             })}
           </tr>
         </thead>
         <tbody>
-          {props.allPlayers.map(player => {
-            return (
-              <tr>
-                <td key={player.Name}>
-                  <span
-                    class="badge badge-setup"
-                    style={{ backgroundColor: player.Class.HexColor }}
-                  >
-                    {player.Name}
-                  </span>
-                </td>
-              </tr>
-            );
-          })}
+          <PlayersList />
         </tbody>
       </table>
     </React.Fragment>
@@ -41,8 +35,7 @@ const SetupDetails = props => {
 
 const mapStateToProps = state => {
   return {
-    selectRaid: state.selectRaid,
-    allPlayers: state.getAllPlayers
+    selectRaid: state.selectRaid
   };
 };
 
