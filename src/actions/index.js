f4a70fb798ff7cb92a0d69cde5b79638a3bf76ea
Michael-MO <Michael-MO@users.noreply.github.com>
2019-09-12 14:46:44 +0200

    Use the new DB class

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,11 @@
+import Firebase from "../apis/firebaseDB";
+
+const db = new Firebase();
+
+export const dbConnection = () => {
+  return db;
+};
+
 export const selectRaidAction = raid => {
   return {
     type: "SELECT_RAID",
@@ -5,20 +13,26 @@ export const selectRaidAction = raid => {
   };
 };
 
-export const getRaidsAction = dbRef => async dispatch => {
-  const response = await dbRef.once("value").then(snapshot => {
-    return snapshot.val();
-  });
+export const getRaidsAction = () => async dispatch => {
+  const response = await db
+    .raids()
+    .once("value")
+    .then(snapshot => {
+      return snapshot.val();
+    });
   dispatch({
     type: "GET_RAIDS",
     payload: response
   });
 };
 
-export const getPlayersAction = dbRef => async dispatch => {
-  const response = await dbRef.once("value").then(snapshot => {
-    return snapshot.val();
-  });
+export const getPlayersAction = () => async dispatch => {
+  const response = await db
+    .players()
+    .once("value")
+    .then(snapshot => {
+      return snapshot.val();
+    });
   dispatch({
     type: "GET_PLAYERS",
     payload: response
@@ -38,22 +52,38 @@ export const createPlayerAction = dbRef => async dispatch => {
   });
 };
 
-export const getClassesAction = dbRef => async dispatch => {
-  const response = await dbRef.once("value").then(snapshot => {
-    return snapshot.val();
-  });
+export const getClassesAction = () => async dispatch => {
+  const response = await db
+    .classes()
+    .once("value")
+    .then(snapshot => {
+      return snapshot.val();
+    });
   dispatch({
     type: "GET_CLASSES",
     payload: response
   });
 };
 
-export const getRolesAction = dbRef => async dispatch => {
+export const getRolesAction = () => async dispatch => {
+  const response = await db
+    .roles()
+    .once("value")
+    .then(snapshot => {
+      return snapshot.val();
+    });
+  dispatch({
+    type: "GET_ROLES",
+    payload: response
+  });
+};
+
+export const getSetups = dbRef => async dispatch => {
   const response = await dbRef.once("value").then(snapshot => {
     return snapshot.val();
   });
   dispatch({
-    type: "GET_ROLES",
+    type: "GET_SETUPS",
     payload: response
   });
 };
