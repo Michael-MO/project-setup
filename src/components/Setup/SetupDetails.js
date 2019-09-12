91ccd481c3d6f3914aa20957c7351114fe55147a
Michael-MO <Michael-MO@users.noreply.github.com>
2019-09-12 14:45:29 +0200

    Redirect user, if the SelectRaid prop is null

--- src/components/Setup/SetupDetails.js
+++ src/components/Setup/SetupDetails.js
@@ -1,49 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 import BossList from "./BossList";
 import { ChangePlayerState } from "../../utils";
 
-const SetupDetails = props => {
-  if (props.Raid) {
-    return (
-      <React.Fragment>
-        <thead>
-          <BossList bosses={props.Raid.Bosses} />
-        </thead>
-        <tbody>
-          {props.Players.map(player => {
-            return (
-              <tr>
-                {props.Raid.Bosses.map(boss => {
-                  return (
-                    <td>
-                      <span
-                        className="badge badge-setup badge-success"
-                        onDoubleClick={e => ChangePlayerState(e)}
-                      ></span>
-                    </td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </React.Fragment>
-    );
-  } else {
-    return (
-      <React.Fragment>
-        <thead>
-          <tr>
-            <th>
-              <h2>Select a raid</h2>
-            </th>
-          </tr>
-        </thead>
-      </React.Fragment>
-    );
+class SetupDetails extends Component {
+  render() {
+    if (this.props.Raid) {
+      return (
+        <React.Fragment>
+          <table id="table-setup" className="table table-sm table-borderless">
+            <thead>
+              <BossList bosses={this.props.Raid.Bosses} />
+            </thead>
+            <tbody>
+              {this.props.Players.map(player => {
+                return (
+                  <tr>
+                    {this.props.Raid.Bosses.map(boss => {
+                      return (
+                        <td>
+                          <span
+                            className="badge badge-setup badge-success"
+                            onDoubleClick={e => ChangePlayerState(e)}
+                          ></span>
+                        </td>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </React.Fragment>
+      );
+    } else {
+      return <Redirect to="/setup" />;
+    }
   }
-};
+}
 
 const mapStateToProps = state => {
   return {
